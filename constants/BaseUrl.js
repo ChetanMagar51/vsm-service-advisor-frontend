b8eea1e47bc6c5e35242ffced68aa0e602b75ec5
chetan magar <sonumagar7264@gmail.com>
2026-06-04 01:36:27 +0530

    url change

--- constants/BaseUrl.js
+++ constants/BaseUrl.js
@@ -1,2 +1,5 @@
 // export const BaseUrl = "https://vehicle-service-management.azurewebsites.net";
-export const BaseUrl = "http://localhost:8082";
+// export const BaseUrl = "http://localhost:8082";
+
+
+export const BaseUrl = "https://vehicle-service-backend-b9zz.onrender.com   ";
